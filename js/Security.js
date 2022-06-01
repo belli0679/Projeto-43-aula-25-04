e2b58426f835c5c68a4f7f0979196d574f06e476
belli0679 <93786545+belli0679@users.noreply.github.com>
2022-06-01 01:28:45 -0300

    Add files via upload

--- js/Security.js
+++ js/Security.js
@@ -32,6 +32,7 @@ class Security {
       if (system.authenticate(accessCode1, this.access1.value())) {
         this.button1.hide();
         this.access1.hide();
+        score++;
         
       }
     });
@@ -40,7 +41,8 @@ class Security {
       if (system.authenticate(accessCode2, this.access2.value())) {
         this.button2.hide();
         this.access2.hide();
-        
+        score++;
+
       }
     });
 
@@ -48,7 +50,7 @@ class Security {
       if (system.authenticate(accessCode3, this.access3.value())) {
         this.button3.hide();
         this.access3.hide();
-        
+        score++;
         
       }
     });
